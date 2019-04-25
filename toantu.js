fa58d00b61155d9623db6e6699d1c40cfd4e17fb
xoan1995 <hoanglapnhue@gmail.com>
2019-04-25 13:29:15 +0800

    thuchanhtoantu

---
+++ toantu.js
@@ -0,0 +1,7 @@
+
+let InputWidth = prompt('Enter the Width');
+let InputHeight = prompt('Enter the Height');
+let Width = parseInt(InputWidth);
+let Height = parseInt(InputHeight);
+let area = Width * Height;
+document.write('Dien tich la' + area);
